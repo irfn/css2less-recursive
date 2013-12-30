f6fa6a783e512c2917fa223654689ed4ecce983c
Irfan Shah <mail.urfi@gmail.com>
2013-12-30 15:40:40 +0530

    script to recursively convert css to less

---
+++ lib/css2less/recursive.rb
@@ -0,0 +1,7 @@
+require "css2less/recursive/version"
+
+module Css2less
+  module Recursive
+    # Your code goes here...
+  end
+end
---
+++ lib/css2less/recursive/version.rb
@@ -0,0 +1,5 @@
+module Css2less
+  module Recursive
+    VERSION = "0.0.1"
+  end
+end
